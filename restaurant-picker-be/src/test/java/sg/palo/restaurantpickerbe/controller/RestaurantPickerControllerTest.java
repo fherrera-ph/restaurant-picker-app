2c3b19285ea676a1d6bc3d2aef930749f8fe52fc
frank herrera <fherrera.ph@gmail.com>
2023-09-25 11:18:11 +0800

    documentation

--- restaurant-picker-be/src/test/java/sg/palo/restaurantpickerbe/controller/RestaurantPickerControllerTest.java
+++ restaurant-picker-be/src/test/java/sg/palo/restaurantpickerbe/controller/RestaurantPickerControllerTest.java
@@ -6,22 +6,25 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import sg.palo.restaurantpickerbe.service.RestaurantPickerService;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = {RestaurantPickerService.class})
 @Import(RestaurantPickerController.class)
-@AutoConfigureRestDocs(outputDir = "target/snippets")
+@AutoConfigureRestDocs
 public class RestaurantPickerControllerTest {
+
 
     @Autowired
     private MockMvc mockMvc;
@@ -30,18 +33,29 @@ public class RestaurantPickerControllerTest {
     private RestaurantPickerService restaurantPickerService;
 
     @Test
-    void shouldReturnOnlyOneResult() throws Exception {
+    void shouldReturnOnlyOneResultFromOneData() throws Exception {
         this.mockMvc.perform(get("/pick")
-                .queryParam("restaurants","mcdonalds"))
+                        .queryParam("restaurants","mcdonalds"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.result").value("mcdonalds"));
     }
-
     @Test
-    void shouldReturnAnyResult() throws Exception {
+    void shouldReturnOnlyOneResultFromMultipleData() throws Exception {
         this.mockMvc.perform(get("/pick")
                 .queryParam("restaurants","mcdonalds,burgerking,wingstop"))
                 .andExpect(status().isOk())
-                .andDo(document("home"));
+                .andExpect(content()
+                        .string(org.hamcrest.Matchers.containsString("{\"result\"")))
+                .andDo(document("restaurant-picker-doc",
+                        preprocessResponse(prettyPrint())));
+    }
+
+
+    @Test
+    void shouldReturnError() throws Exception {
+        this.mockMvc.perform(get("/pick"))
+                .andExpect(status().is4xxClientError())
+                .andDo(document("restaurant-picker-doc-error",
+                        preprocessResponse(prettyPrint())));
     }
 }
