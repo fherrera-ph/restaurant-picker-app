5de483fa5e6a8075de37e921dad9261e9fdcadbb
frank herrera <fherrera.ph@gmail.com>
2023-09-26 19:34:05 +0800

    added tests changes with mock

--- restaurant-picker-fe/src/List.test.js
+++ restaurant-picker-fe/src/List.test.js
@@ -1,19 +1,59 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw'; 
 import List from './List';
 
-test('renders List component', () => {
-  const { getByText, getByPlaceholderText } = render(<List />);
+const server = setupServer(
+  rest.get('http://localhost:8080/restaurantpicker/pick?restaurants=', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({ result: 'Mocked API response' })
+    );
+  })
+);
 
-  // Check if the component renders properly
-  const headerElement = getByText('List the Restaurants where you want to eat');
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
+
+test('renders List component', async () => {
+  render(<List />);
+
+  const headerElement = screen.getByText('List the Restaurants where you want to eat');
   expect(headerElement).toBeInTheDocument();
 
-  const inputElement = getByPlaceholderText('Add an item');
-  const addButton = getByText('Add');
+  const inputElement = screen.getByPlaceholderText('Add an item');
+  const addButton = screen.getByText('Add');
   fireEvent.change(inputElement, { target: { value: 'Test Item' } });
   fireEvent.click(addButton);
 
-  const listItem = getByText('Test Item');
+  const listItem = screen.getByText('Test Item');
   expect(listItem).toBeInTheDocument();
+
+
+  server.use(
+    rest.get('http://localhost:8080/restaurantpicker/pick?restaurants=', (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({ result: 'Mocked API response' })
+      );
+    })
+  );
+
+  const submitButton = screen.getByText('Submit');
+  fireEvent.click(submitButton);
+
+  // Check if the API result is displayed
+  const apiResult = await screen.findByText('You should eat at');
+  expect(apiResult).toBeInTheDocument();
+  expect(apiResult).toHaveTextContent('Mocked API response');
 });
